4695c3ca0e9a2663fece19e316291ef3899df446
Ubuntu <ubuntu@cpu04.maas>
2019-04-28 02:56:45 +0000

    0.02

--- utils.py
+++ utils.py
@@ -5,30 +5,30 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 print(tf.__version__)
 
-def show_images(images, labels, class_names, predicts=None):
-    if images.shape[0] > 9:
-        print("Pls use less than 9 images for demonstration.")
-        return
+def show_images(dataset, class_names, predicts=None):
     fig, axes = plt.subplots(3, 3)
     fig.subplots_adjust(hspace=0.8, wspace=0.4)
-    if len(labels.shape) > 1:
-        labels = np.squeeze(labels)
-    
-    for i, ax in enumerate(axes.flat):
-        if i >= images.shape[0]:
-            ax.axis('off')
-            continue
-        ax.imshow(images[i])
+    for i, (image, label) in enumerate(dataset):
+        if i>= 9: 
+            break
+        if image.shape[-1] == 1:
+            revised_image = np.squeeze(image)
+        else:
+            revised_image = image
+        axes.flat[i].imshow(revised_image)
         # Name of the true class.
-        label_name = class_names[labels[i]]
+        label_name = class_names[label]
         
         # Show true and predicted classes.
         if predicts is None:
-            ax.set_xlabel("True : {0}".format(label_name))
+            axes.flat[i].set_xlabel("True : {0}".format(label_name))
         else:
             predict_name = class_names[predicts[i]]
-            ax.set_xlabel("True: {0}\nPred: {1}".format(label_name, predict_name))
+            axes.flat[i].set_xlabel("True: {0}\nPred: {1}".format(label_name, predict_name))
         # Remove ticks from the plot.
-        ax.set_xticks([])
-        ax.set_yticks([])
+        axes.flat[i].set_xticks([])
+        axes.flat[i].set_yticks([])
+    while(i < 8):
+        i += 1
+        axes.flat[i].axis('off')
     plt.show()
